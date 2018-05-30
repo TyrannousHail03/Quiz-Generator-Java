b0d36b270289d74af9121bfdf3b8c9954e3d10eb
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2018-05-30 15:26:36 -0400

    v0.0.1
    Added JavaDocs to Quiz Generator App

--- Quiz Generator App/QG_App_Buttons.java
+++ Quiz Generator App/QG_App_Buttons.java
@@ -1,9 +1,11 @@
 public class QG_App_Buttons
 {
+   /** Initiates the Quiz Generator when pressed. */
    public static void CreateQuiz()
    {
       //Button that initates the Quiz Generator
    }
+   /** Provides a list of previously created quizzes for users to take. */
    public static void OpenQuiz()
    {
       //Provides a list of quiz names for the user to initiate - will want to write name of quiz to this document
--- Quiz Generator App/QG_App_Driver.java
+++ Quiz Generator App/QG_App_Driver.java
@@ -1,5 +1,7 @@
 public class QG_App_Driver
 {
+   /** Main method for Quiz Generator application. 
+       @param args Just standard input for main Java method.*/
    public static void main(String[] args)
    {
       //Driver code for the Quiz Generator App
