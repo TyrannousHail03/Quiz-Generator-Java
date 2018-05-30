63acfbe6cf9af96801c97418b83576bb226481fc
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2018-05-30 15:13:36 -0400

    v0.0.1
    Added JavaDocs documentation to the Quiz Generator classes.

--- Quiz Generator/AnswerGen.java
+++ Quiz Generator/AnswerGen.java
@@ -1,12 +1,17 @@
+/** Class for generation of answer code in target file. 
+    @version 0.0.1
+    @author TyrannousHail03 */
 public class AnswerGen
 {
    int NUMITEMS = 0;
+   /** Generates the code for a single answer for a single question answer.   */
    public static void SingleAnswer()
    {
-      //Generates code for the single answer for a single answer question. 
+      
    }
+   /**Generates code for multiple answers (in an array) for a multiple answer question*/
    public static void MultipleAnswer()
    {
-      //Generates code for multiple answers (in an array) for a multiple answer question
+      
    }
 }
--- Quiz Generator/BaseFile.java
+++ Quiz Generator/BaseFile.java
@@ -1,16 +1,29 @@
-
+/** Class for Quiz Generator output file base code.
+    @author TyrannousHail03
+    @version 0.0.1 */
 public class BaseFile
 {
-   public void start()
+   String Filename;
+   /** Generates code that imports necessary methods, inital declaration and curly bracket, and indented main method. 
+       @param filename Takes user input for filename and stores as string Filename for later reference.a */
+   public void start(String filename)
    {
+      Filename = filename;
       //Imports methods necessary to run program
       //Writes public class [filename] etc.
       //Curly bracket then main method
       //Curly bracket for main method
       //End of start method
    }
+   /** Adds in the ending curly brackets to the file */
    public void end()
    {
       //Adds in ending curly brackets
+   }
+   /** Returns the filename to the requestor.  
+       @return Filename Returns name of written file to requestor.*/
+   public String Filename()
+   {
+      return Filename;
    }
 }
